e86289acff453edc7666cb68b1ad305df7029dd6
Jim Breslin <jbreslin33@gmail.com>
2018-07-27 10:51:05 -0400

    player

---
+++ player/Player.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var playerColor = Color(0,0,1)
+var startPostion = Vector2(200,200)
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func drawPlayer():
+	draw_circle(startPostion,5,playerColor)
+	
+func _draw():
+	drawPlayer()
