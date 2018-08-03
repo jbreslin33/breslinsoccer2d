f6b0591ccd9693c85fc3cf03dd5559e0fae26879
Jim Breslin <jbreslin@gmail.com>
2018-08-02 21:52:11 -0400

    controllerPlayer

--- player/Player.gd
+++ player/Player.gd
@@ -16,8 +16,14 @@ const WALK_SPEED = 70
 
 var velocity = Vector2()
 
+#team
+var mTeam = 0
+
+#main
+var mMain = 0
+
 func _init():
-	
+
 	#steering
 	mSteeringBehaviors = SteeringBehaviors.new(self)
 
@@ -28,22 +34,31 @@ func _init():
 
 
 func _physics_process(delta):
-    velocity.y += delta
+	velocity.y += delta
+	if (mMain.mControllerPlayer == self):
+		if Input.is_action_pressed("ui_left"):
+			velocity.x = -WALK_SPEED
+		elif Input.is_action_pressed("ui_right"):
+			velocity.x =  WALK_SPEED
+		else:
+			velocity.x = 0
+		if Input.is_action_pressed("ui_up"):
+			velocity.y = -WALK_SPEED
+		elif Input.is_action_pressed("ui_down"):
+			velocity.y =  WALK_SPEED
+		else:
+			velocity.y = 0
+    
+	
+	# We don't need to multiply velocity by delta because MoveAndSlide already takes delta time into account.
 
-    if Input.is_action_pressed("ui_left"):
-        velocity.x = -WALK_SPEED
-    elif Input.is_action_pressed("ui_right"):
-        velocity.x =  WALK_SPEED
-    else:
-        velocity.x = 0
-    if Input.is_action_pressed("ui_up"):
-        velocity.y = -WALK_SPEED
-    elif Input.is_action_pressed("ui_down"):
-        velocity.y =  WALK_SPEED
-    else:
-        velocity.y = 0
-    # We don't need to multiply velocity by delta because MoveAndSlide already takes delta time into account.
+	# The second parameter of move_and_slide is the normal pointing up.
+	# In the case of a 2d platformer, in Godot upward is negative y, which translates to -1 as a normal.
+	move_and_slide(velocity, Vector2(0, -1))
+	
+func setTeam(team):
+	mTeam = team
 
-    # The second parameter of move_and_slide is the normal pointing up.
-    # In the case of a 2d platformer, in Godot upward is negative y, which translates to -1 as a normal.
-    move_and_slide(velocity, Vector2(0, -1))
+func setMain(main):
+	mMain = main
+	
--- team/team.gd
+++ team/team.gd
@@ -1,7 +1,13 @@
 extends Object
 
 var mMain = 0
+var mPlayerArray = []
 
 func _init(main):
 	mMain = main
+	pass
+
+func addPlayers():
+	
+	
 	pass
