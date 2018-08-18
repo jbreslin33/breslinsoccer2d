2a107c3510a854e5b6af3f7e82948b12304da0ca
Jim Breslin <jbreslin33@gmail.com>
2018-08-17 22:11:06 -0400

    fixed look at

--- player/Player.gd
+++ player/Player.gd
@@ -99,7 +99,7 @@ func _physics_process(delta):
 		mVelocity = mSteeringBehaviors.mSteeringForce * mRunSpeed
 		
 	if (mBall.mPlayer == self):
-		print("x:", mVelocity.x, "y:",mVelocity.y)		
+		#print("x:", mVelocity.x, "y:",mVelocity.y)		
 		if (mVelocity.x == 0  && mVelocity.y < 0):
 			mSprite.play("dribble_0")
 			
@@ -159,9 +159,7 @@ func getShootingPosition():
 	pass	
 	
 func _on_BallArea2D_body_entered(body):
-	print("hit",mRunSpeed)
 	if (body == self):
-		print("body is self")
 		mBall.mPlayer = self
 		if (body.mTeam == mMain.mHomeTeam):
 			print("home team hit ball")
