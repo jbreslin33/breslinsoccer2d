1018337651cba5f004766bf8d1383b5c65edef58
Jim Breslin <jbreslin33@gmail.com>
2018-08-17 22:32:21 -0400

    hide ball

--- ball/Ball.gd
+++ ball/Ball.gd
@@ -1,22 +1,17 @@
 extends RigidBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 var mPlayer = null
 var mMain = null
 var mPlayerTimedOut = null
 var mVelocity = Vector2(0,0)
 var mFriction = 0.05
 var mHideVector = Vector2(0,0)
+
 #physics
 var mInitialVelocity = Vector2(0,0)
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	set_gravity_scale(0)
-	pass # Replace with function body.
 
 func kick(offset,impulse):
 	pass
@@ -26,5 +21,5 @@ func _physics_process(delta):
 		show()
 	else:
 		hide()
-	pass
+
 
