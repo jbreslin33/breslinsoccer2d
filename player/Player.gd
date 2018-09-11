48f8479aa155ae2ce900b38d4c563049ea2a3481
Jim Breslin <jbreslin33@gmail.com>
2018-09-11 10:45:28 -0400

    rotation!

--- player/Player.gd
+++ player/Player.gd
@@ -154,7 +154,12 @@ func _physics_process(delta):
 				enableFeelerCollisions()
 		else:
 			enableFeelerCollisions()
-			
+
+	#test rotate
+	if (mBall.mPlayer != self):
+		var p = Vector2(mBall.global_position.x,mBall.global_position.y)
+		look_at(p)
+
 	move_and_slide(mVelocity, Vector2(0, -1))
 	
 func setTeam(team):
